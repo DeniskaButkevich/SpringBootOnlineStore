52e8a47f28db102d1ffe43f110afcc186ad1331b
denis <deniskabutkevich@gmail.com>
2020-03-02 23:03:04 +0300

    started create cart

--- src/main/java/com/dez/predesign/controller/ForAll.java
+++ src/main/java/com/dez/predesign/controller/ForAll.java
@@ -1,8 +1,11 @@
 package com.dez.predesign.controller;
 
+import com.dez.predesign.data.User;
 import com.dez.predesign.data.catalog.Category;
 import com.dez.predesign.repository.CategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -22,5 +25,20 @@ public class ForAll {
     @ModelAttribute(name = "categoriesLevelTwo")
     public List<Category> setCategoriesLevelTwo() {
         return categoryRepo.findByLevel(2);
+    }
+
+    @ModelAttribute(name = "authorizedUserId")
+    public Integer getUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Integer userId;
+
+        if(authentication.getPrincipal().toString().equals("anonymousUser")){
+            userId = -1;
+        }else{
+            User customUser = (User)authentication.getPrincipal();
+            userId = customUser.getId();
+        }
+
+        return userId;
     }
 }
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,78 @@
+var cart = {}; //моя корзина
+
+$('document').ready(function(){
+    checkCart();
+});
+
+function addToCart(productId) {
+
+    if (cart[productId] == undefined ) {
+        cart[productId] = 1;
+        localStorage.setItem('cart', JSON.stringify(cart));
+
+
+        var elem = document.getElementsByClassName("product_cart");
+        for (var i = 0; i < elem.length; i++){
+            elem.item(i).textContent = "In the cart: " + Object.keys(cart).length.toString();
+        }
+
+        var addedElem = document.getElementsByClassName(productId);
+        for (var i = 0; i < addedElem.length; i++){
+            addedElem.item(i).parentElement
+                .setAttribute('onclick','location.href=\'/cart\'');
+            addedElem.item(i).textContent  = "Added";
+        }
+
+        document.getElementById("scroll-cart").style.display = "block";
+    }
+}
+
+function removeFromCart() {
+
+    for (var key in cart){
+        delete cart[key];
+
+        var addedElem = document.getElementsByClassName(key);
+        for (var i = 0; i < addedElem.length; i++){
+            addedElem.item(i).parentElement
+                .setAttribute('onclick','addToCart(' + key +')');
+            addedElem.item(i).textContent = "Add to Cart";
+        }
+    }
+
+    var elem = document.getElementsByClassName("product_cart");
+    for (var i = 0; i < elem.length; i++){
+        elem.item(i).textContent = "";
+    }
+
+    cart = {};
+    localStorage.setItem('cart', JSON.stringify(cart));
+    document.getElementById("scroll-cart").style.display = "none";
+}
+
+function checkCart(){
+    //проверяю наличие корзины в localStorage;
+    if (localStorage.getItem('cart') != null
+        && localStorage.getItem('cart').length != 2) {
+        cart = JSON.parse (localStorage.getItem('cart'));
+
+        var elem = document.getElementsByClassName("product_cart");
+        for (var i = 0; i < elem.length; i++){
+            elem.item(i).textContent = "In the cart: " + Object.keys(cart).length.toString();
+        }
+
+        document.getElementById("scroll-cart").style.display = "block";
+
+        for (var key in cart){
+
+            var addedElem = document.getElementsByClassName(key);
+            for (var i = 0; i < addedElem.length; i++){
+                addedElem.item(i).parentElement
+                    .setAttribute('onclick','location.href=\'/cart\'');
+                addedElem.item(i).textContent  = "Added";
+            }
+        }
+    }
+
+
+}
